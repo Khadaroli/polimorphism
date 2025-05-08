f50452d043295e0e09b2550147e16fcb556347a4
khadaroli <khadaroli@gmail.com>
2025-05-08 15:42:51 +0530

    Selenium Pro Added

---
+++ src/demoselenium/introSelenium.java
@@ -0,0 +1,22 @@
+package demoselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class introSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//WebDriver driver1 = new WebDriver();			
+		WebDriver driver = new ChromeDriver();		
+		WebDriver driver1 = new FirefoxDriver();
+		WebDriver drvier2 = new EdgeDriver();
+		
+		
+	}
+
+}
